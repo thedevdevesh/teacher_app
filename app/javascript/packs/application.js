54017bcda639ae95898d95cc19bd4f4c84e3c2b6
devesh140901 <deveshkumarsingh99@gmail.com>
2024-10-28 17:15:20 +0530

    update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,7 @@
-import "bootstrap";
-import "jquery";
 import Rails from "@rails/ujs";
-
 Rails.start();
-window.$ = window.jQuery = require("jquery");
+
+import jQuery from "jquery";
+window.$ = window.jQuery = jQuery;
+
+import "bootstrap";
